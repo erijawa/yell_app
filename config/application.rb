2d41249d62165c1389f2447f8b719064cd7e1eb5
erijawa <e.akgw0913@gmail.com>
2024-06-03 10:51:44 +0900

    ホスト名のホワイトリスト追加

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module YellApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'yell-app-vcr7.onrender.com'
   end
 end
