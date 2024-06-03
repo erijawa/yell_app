71c2ee18b98a1d9397b8ceb83e0b29203426191e
erijawa <e.akgw0913@gmail.com>
2024-06-03 12:51:07 +0900

    edit sh file

--- app/controllers/yell_controller.rb
+++ app/controllers/yell_controller.rb
@@ -1,17 +1,24 @@
 class YellController < ApplicationController
-  def top
-  end
+  def top; end
 
   def show
+    @yell = Yell.all.sample
   end
 
   def new
   end
 
   def create
-
+    yell = Yell.new(yell_params)
+    yell.save!
+    redirect_to yell_success_path
   end
 
-  def success
+  def success; end
+
+  private
+
+  def yell_params
+    params.require(:yell).permit(:name, :body)
   end
 end
